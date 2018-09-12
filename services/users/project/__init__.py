27a3003e78f3985bf17f6e2b607c2358c573c6fc
nicolas <nicolas@localhost.localdomain>
2018-09-12 14:38:12 +0200

    Follow tutorial second part. Begin refactorisation.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,24 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-# set configuration
-app.config.from_object('project.config.DevelopmentConfig')
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong'
-        })
+db = SQLAlchemy()
+
+
+def create_app(script_info=None):
+    app = Flask(__name__)
+    # set configuration
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    db.init_app(app)
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flaks cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
