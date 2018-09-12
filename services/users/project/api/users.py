bc071618427dfa06d7b497f1b9a84084eb6f8939
nicolas <nicolas@localhost.localdomain>
2018-09-12 15:21:18 +0200

    Get user details by id number.

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -41,3 +41,18 @@ def add_user():
         return jsonify(response_object), 400
 
 
+@users_blueprint.route('/users/<user_id>', methods=['GET'])
+def get_single_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    response_object = {
+        'status': 'success',
+        'data': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'active': user.active
+        }
+    }
+    return jsonify(response_object), 200
+
+
