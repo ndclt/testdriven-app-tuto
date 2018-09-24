f18004a537fa2cc98ebe3197ff3900b3fa3dba81
ndclt <nicolas.duclert@estaca.eu>
2018-09-24 16:17:35 +0200

    Add password into User init.

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.sql import func
-from project import db
+from project import db, bcrypt
 
 
 class User(db.Model):
@@ -12,9 +12,10 @@ class User(db.Model):
     active = db.Column(db.Boolean(), default=True, nullable=False)
     created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
 
-    def __init__(self, username, email):
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
+        self.password = bcrypt.generate_password_hash(password).decode()
 
     def to_json(self):
         return {
