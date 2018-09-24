bad56283e98cfebb0d9e9041d049d40c3c62d265
ndclt <nicolas.duclert@estaca.eu>
2018-09-24 10:26:33 +0200

    Add an user to the backend database from React.

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -9,22 +9,31 @@ class App extends Component {
     constructor(){
 	super();
 	this.state = {
-	    users: []
+	    users: [],
+	    username: '',
+	    email: '',
 	};
+	this.addUser = this.addUser.bind(this);
+	this.handleChange = this.handleChange.bind(this);
     }
     componentDidMount(){
 	this.getUsers();
     };
     render(){
 	return(
-	    <section className="section">
-	    <div className="container">
+		<section className="section">
+		<div className="container">
 		<div className="columns">
 		<div className="column is-half">
 		<br/>
-	    <h1 className="title is1 is1">All Users</h1>
+		<h1 className="title is1 is1">All Users</h1>
 		<hr/><br/>
-		<AddUser/>
+		<AddUser
+	    username={this.state.username}
+	    email={this.state.email}
+	    addUser={this.addUser}
+	    handleChange={this.handleChange}
+	    />
 		<hr/><br/>
 		<UsersList users={this.state.users}/>
 	    </div>
@@ -38,6 +47,28 @@ class App extends Component {
 	    .then((res) => { this.setState({users: res.data.data.users}); })
 	    .catch((err) => { console.log(err); });
     }
+
+    addUser(event){
+	event.preventDefault();
+	const data = {
+	    username: this.state.username,
+	    email: this.state.email
+	};
+	axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+	    .then((res) => {
+		this.getUsers();
+		this.setState({username: '', email: ''});
+	    })
+	    .catch((err) => { console.log(err); });
+	console.log('sanity check');
+	console.log(this.state);
+    };
+
+    handleChange(event){
+	const obj = {};
+	obj[event.target.name] = event.target.value;
+	this.setState(obj)
+    };
 };
 
 ReactDOM.render(
